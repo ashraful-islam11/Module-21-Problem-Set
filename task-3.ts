29e47616597613e3a6837766f8002d5ac35dd3d5
Ashraful Islam <ashraful171790@gmail.com>
2026-08-15 21:55:17 +0600

    third task is done

---
+++ task-3.ts
@@ -0,0 +1,11 @@
+// todo :   Module no - 3. 
+// Problem Statement:
+// একটি array বানাও যেখানে student-এর নাম থাকবে এবং কতজন আছে সেটা কনসোল করবে।
+// Input:
+// ["Amina", "Rahim", "Karim", "Salma", "Rafi"]
+// Output:
+// Total student: 5
+
+
+let studentsName : string[] = ["Amina", "Rahim", "Karim", "Salma", "Rafi"];
+console.log(`Total student: ${studentsName.length}`);
