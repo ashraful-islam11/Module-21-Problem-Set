3bb6329aa062e425e14564068a3501a329384f86
Ashraful Islam <ashraful171790@gmail.com>
2026-08-15 22:04:33 +0600

    fourt is done

---
+++ task-4.js
@@ -0,0 +1,10 @@
+"use strict";
+// todo :   Problem no -4. 
+// Problem Statement:
+// একটি tuple বানাও যেখানে একজন মানুষের নাম আর বয়স থাকবে।
+// Input:
+// ["Amina", 22]
+// Output:
+// Amina is 22 years old.
+let userInfo = ["Amina", 22];
+console.log(`${userInfo[0]} is ${userInfo[1]} years old`);
