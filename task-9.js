4233843776826cc675be2e6463a8507d96467b45
Ashraful Islam <ashraful171790@gmail.com>
2026-08-15 22:57:46 +0600

    problem - 9 is solve

---
+++ task-9.js
@@ -0,0 +1,12 @@
+"use strict";
+// todo:   problem no - 9. 
+// Problem Statement:
+// একটি number positive না negative তা ternary operator দিয়ে বের করো।
+// Input:
+// num = 5
+// Output:
+// Positive
+let num = 5;
+// if( num > 0) ? 'Positive' : '';
+let result = num > 0 ? 'Positive' : '';
+console.log(result);
---
+++ task-9.ts
@@ -0,0 +1,15 @@
+// todo:   problem no - 9. 
+// Problem Statement:
+// একটি number positive না negative তা ternary operator দিয়ে বের করো।
+// Input:
+// num = 5
+// Output:
+// Positive
+
+let num : number = 5; 
+
+// if( num > 0) ? 'Positive' : '';
+let result = num > 0 ? 'Positive' : '';
+console.log(result);
+
+
